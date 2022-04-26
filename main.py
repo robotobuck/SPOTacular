92c10a9c5b58cc5ada3415c964c6d0511303e5d2
Grant Bruneel <grant.ray.bruneel@aggiemail.usu.edu>
2022-04-26 17:54:17 -0600

    comments to clear it up

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import randomRobotData
 
+# This will be replaced when real data becomes available
+# This should be called again for every new section of land to survey
 robotData = randomRobotData.generateRandomData(5)
 print("Generated Robot Data: \n", robotData)
