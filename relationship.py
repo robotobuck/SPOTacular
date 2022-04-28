7afd87dc741a93cf56fc7c24f2ae1b3845314809
Grant Bruneel <grant.ray.bruneel@aggiemail.usu.edu>
2022-04-27 19:38:50 -0600

    chore: scale relationships easier by using methods

--- relationship.py
+++ relationship.py
@@ -13,13 +13,17 @@ class RelationshipBetweenRobotAndArea:
 
   def findAbilityAndEfficiency(self):
     if self.__robot.getRobotType() == 'submarine':
-      # Submarine is made to survey water, thus ability and efficiency are maxed out for submarine in water
-      if self.__area.getEnvironmentType() == 'water':
-        self.__ability = 10
-        self.__efficiency = 100
-      # Submarine cannot do anything if the area is out of the water, its ability stays at 0
+      self.submarineAbility()
+
 
   def displayRelationshipInfo(self):
     print(f"Relationship of robot type \'{self.__robot.getRobotType()}\' and \'{self.__area.getEnvironmentType()}\'")
     print(f"Ability: {self.__ability}")
     print(f"Efficiency: {self.__efficiency}")
+    
+  def submarineAbility(self):
+   # Submarine is made to survey water, thus ability and efficiency are maxed out for submarine in water
+    if self.__area.getEnvironmentType() == 'water':
+        self.__ability = 10
+        self.__efficiency = 100
+    # Submarine cannot do anything if the area is out of the water, its ability stays at 0
